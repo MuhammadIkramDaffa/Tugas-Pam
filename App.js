7bf46b1553acbd2467921cce36edaad63f0685ea
MuhammadIkramDaffa <muhammad.118140099@student.itera.ac.id>
2021-03-09 19:18:07 +0700

    Upload Tugas 1

---
+++ App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux'
+import Store from './src/redux/ice/store'
+import Ice from './src/component/ice/container'
+export default function App() {
+  return (
+    <Provider store={Store}>
+      <Ice /> 
+    </Provider>
+    
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
